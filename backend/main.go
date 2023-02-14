a4aee80cb6b7b950c1facc0503ff003de67f82a9
LuisBarrera23 <luisbarrera5662@gmail.com>
2023-02-13 23:49:13 -0600

    back y front terminado falta db

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,168 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+)
+
+type saludo struct {
+	Saludo string
+}
+
+type resultado struct {
+	Resultado string
+}
+
+type formato struct {
+	Simbolo string
+	Numero1 string
+	Numero2 string
+}
+
+type registros struct {
+	Numero1   string
+	Numero2   string
+	Operacion string
+	Resultado string
+	Fecha     string
+}
+
+var operaciones []registros
+
+func getVerificacion(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "application/json")
+	enviar := saludo{Saludo: "Bienvenido el servidor esta ejecutandose correctamente!!"}
+	json.NewEncoder(res).Encode(enviar)
+}
+
+func endPointRegistros(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(res).Encode(operaciones)
+}
+
+func endPointSuma(res http.ResponseWriter, req *http.Request) {
+	entrada := &formato{}
+	_ = json.NewDecoder(req.Body).Decode(entrada)
+	valor1, error1 := strconv.ParseFloat(entrada.Numero1, 64) //convertir de string a float64
+	valor2, error2 := strconv.ParseFloat(entrada.Numero2, 64) //convertir de string a float64
+	var respuesta resultado
+	if error1 != nil || error2 != nil {
+		respuesta := resultado{Resultado: "Error"}
+		res.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(res).Encode(respuesta)
+		return
+	}
+
+	suma := strconv.FormatFloat(valor1+valor2, 'f', -1, 64) //convertir de float a string
+	//fmt.Println(suma)
+
+	respuesta = resultado{Resultado: suma}
+
+	actual := time.Now()
+	actualstring := actual.Format("02/01/2006 15:04:05")
+	registro := registros{Numero1: entrada.Numero1, Numero2: entrada.Numero2, Operacion: entrada.Simbolo, Resultado: suma, Fecha: actualstring}
+	operaciones = append(operaciones, registro)
+	res.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(res).Encode(respuesta)
+
+}
+
+func endPointResta(res http.ResponseWriter, req *http.Request) {
+	entrada := &formato{}
+	_ = json.NewDecoder(req.Body).Decode(entrada)
+	valor1, error1 := strconv.ParseFloat(entrada.Numero1, 64) //convertir de string a float64
+	valor2, error2 := strconv.ParseFloat(entrada.Numero2, 64) //convertir de string a float64
+	var respuesta resultado
+	if error1 != nil || error2 != nil {
+		respuesta := resultado{Resultado: "Error"}
+		res.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(res).Encode(respuesta)
+		return
+	}
+
+	resta := strconv.FormatFloat(valor1-valor2, 'f', -1, 64) //convertir de float a string
+	//fmt.Println(resta)
+
+	respuesta = resultado{Resultado: resta}
+	actual := time.Now()
+	actualstring := actual.Format("02/01/2006 15:04:05")
+	registro := registros{Numero1: entrada.Numero1, Numero2: entrada.Numero2, Operacion: entrada.Simbolo, Resultado: resta, Fecha: actualstring}
+	operaciones = append(operaciones, registro)
+	res.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(res).Encode(respuesta)
+
+}
+
+func endPointMultiplicacion(res http.ResponseWriter, req *http.Request) {
+	entrada := &formato{}
+	_ = json.NewDecoder(req.Body).Decode(entrada)
+	valor1, error1 := strconv.ParseFloat(entrada.Numero1, 64) //convertir de string a float64
+	valor2, error2 := strconv.ParseFloat(entrada.Numero2, 64) //convertir de string a float64
+	var respuesta resultado
+	if error1 != nil || error2 != nil {
+		respuesta := resultado{Resultado: "Error"}
+		res.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(res).Encode(respuesta)
+		return
+	}
+
+	multiplicacion := strconv.FormatFloat(valor1*valor2, 'f', -1, 64) //convertir de float a string
+	//fmt.Println(multiplicacion)
+
+	respuesta = resultado{Resultado: multiplicacion}
+	actual := time.Now()
+	actualstring := actual.Format("02/01/2006 15:04:05")
+	registro := registros{Numero1: entrada.Numero1, Numero2: entrada.Numero2, Operacion: entrada.Simbolo, Resultado: multiplicacion, Fecha: actualstring}
+	operaciones = append(operaciones, registro)
+	res.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(res).Encode(respuesta)
+
+}
+
+func endPointDivision(res http.ResponseWriter, req *http.Request) {
+	entrada := &formato{}
+	_ = json.NewDecoder(req.Body).Decode(entrada)
+	valor1, error1 := strconv.ParseFloat(entrada.Numero1, 64) //convertir de string a float64
+	valor2, error2 := strconv.ParseFloat(entrada.Numero2, 64) //convertir de string a float64
+	var respuesta resultado
+	if error1 != nil || error2 != nil || valor2 == 0 {
+		respuesta := resultado{Resultado: "Error"}
+		res.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(res).Encode(respuesta)
+		return
+	}
+
+	division := strconv.FormatFloat(valor1/valor2, 'f', -1, 64) //convertir de float a string
+	//fmt.Println(division)
+
+	respuesta = resultado{Resultado: division}
+	actual := time.Now()
+	actualstring := actual.Format("02/01/2006 15:04:05")
+	registro := registros{Numero1: entrada.Numero1, Numero2: entrada.Numero2, Operacion: entrada.Simbolo, Resultado: division, Fecha: actualstring}
+	operaciones = append(operaciones, registro)
+	res.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(res).Encode(respuesta)
+
+}
 
 func main() {
-	fmt.Println("Hola mundo xd")
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", getVerificacion).Methods("GET")
+	router.HandleFunc("/registros", endPointRegistros).Methods("GET")
+	router.HandleFunc("/suma", endPointSuma).Methods("POST")
+	router.HandleFunc("/resta", endPointResta).Methods("POST")
+	router.HandleFunc("/multiplicacion", endPointMultiplicacion).Methods("POST")
+	router.HandleFunc("/division", endPointDivision).Methods("POST")
+
+	parametro1 := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization", "Access-Control-Allow-Origin"}) // aceptar todos los heders del fetch
+	parametro2 := handlers.AllowedMethods([]string{"GET", "POST", "HEAD", "OPTIONS"})                                                   // indico que metodos aceptare
+	parametro3 := handlers.AllowedOrigins([]string{"*"})                                                                                //que acepte todos los orignes
+	http.ListenAndServe(":5000", handlers.CORS(parametro1, parametro2, parametro3)(router))
+
 }
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import './App.css';
 
 function App() {
@@ -6,20 +6,160 @@ function App() {
 
   const [valor1, setValor1] = useState('');
   const [valor2, setValor2] = useState('');
+  const LabelResultado=useRef(null);
 
   const verificarNumero1 = event => {
     const { value } = event.target;
-    if (!isNaN(value)) {  //verificamos si valor no es un numero seteamos el valor anterior
+    if (!isNaN(value) || value ==='-') {  //verificamos si valor no es un numero seteamos el valor anterior
       setValor1(value);    //es decir ignoramos el valor que se ingreso (una letra)
     }
   };
 
   const verificarNumero2 = event => {
     const { value } = event.target;
-    if (!isNaN(value)) {  //verificamos si valor no es un numero seteamos el valor anterior
+    if (!isNaN(value) || value ==='-') {  //verificamos si valor no es un numero seteamos el valor anterior
       setValor2(value);    //es decir ignoramos el valor que se ingreso (una letra)
     }
   };
+
+  async function suma() {
+
+    if(valor1 === ""&&valor2 === ""){
+      alert("Por favor ingrese ambos numeros")
+    }else if(valor1 === ""){
+      alert("Por favor ingrese el primer numero")
+    }else if(valor2 === ""){
+      alert("Por favor ingrese el segundo numero")
+    }else{
+      const jsonDatos={"Simbolo":"+","Numero1":valor1,"Numero2":valor2}
+      console.log(jsonDatos)
+
+      try {
+        const response = await fetch('http://localhost:5000/suma', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' ,'Access-Control-Allow-Origin': '*',},
+          body: JSON.stringify(jsonDatos)
+        });
+        const result = await response.json();
+        LabelResultado.current.innerHTML=result.Resultado;
+        //console.log('Success:', result);
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    }
+
+    
+  }
+
+  async function resta() {
+
+    if(valor1 === ""&&valor2 === ""){
+      alert("Por favor ingrese ambos numeros")
+    }else if(valor1 === ""){
+      alert("Por favor ingrese el primer numero")
+    }else if(valor2 === ""){
+      alert("Por favor ingrese el segundo numero")
+    }else{
+      const jsonDatos={"Simbolo":"-","Numero1":valor1,"Numero2":valor2}
+      console.log(jsonDatos)
+
+      try {
+        const response = await fetch('http://localhost:5000/resta', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' ,'Access-Control-Allow-Origin': '*',},
+          body: JSON.stringify(jsonDatos)
+        });
+        const result = await response.json();
+        LabelResultado.current.innerHTML=result.Resultado;
+        //console.log('Success:', result);
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    }
+
+    
+  }
+
+  async function multiplicacion() {
+
+    if(valor1 === "" && valor2 === ""){
+      alert("Por favor ingrese ambos numeros")
+    }else if(valor1 === ""){
+      alert("Por favor ingrese el primer numero")
+    }else if(valor2 === ""){
+      alert("Por favor ingrese el segundo numero")
+    }else{
+      const jsonDatos={"Simbolo":"*","Numero1":valor1,"Numero2":valor2}
+      console.log(jsonDatos)
+
+      try {
+        const response = await fetch('http://localhost:5000/multiplicacion', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' ,'Access-Control-Allow-Origin': '*',},
+          body: JSON.stringify(jsonDatos)
+        });
+        const result = await response.json();
+        LabelResultado.current.innerHTML=result.Resultado;
+        //console.log('Success:', result);
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    }
+
+    
+  }
+
+  async function division() {
+
+    if(valor1 === ""&&valor2 === ""){
+      alert("Por favor ingrese ambos numeros")
+    }else if(valor1 === ""){
+      alert("Por favor ingrese el primer numero")
+    }else if(valor2 === ""){
+      alert("Por favor ingrese el segundo numero")
+    }else{
+      const jsonDatos={"Simbolo":"/","Numero1":valor1,"Numero2":valor2}
+      console.log(jsonDatos)
+
+      try {
+        const response = await fetch('http://localhost:5000/division', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' ,'Access-Control-Allow-Origin': '*',},
+          body: JSON.stringify(jsonDatos)
+        });
+        const result = await response.json();
+        LabelResultado.current.innerHTML=result.Resultado;
+        //console.log('Success:', result);
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    }
+
+    
+  }
+
+  const [dataLogs, setLogs] = useState(null);
+  const tablaLogs=useRef(null);
+
+  async function logs() {
+    const response = await fetch('http://localhost:5000/registros');
+    const data = await response.json();
+    setLogs(data);
+    console.log(dataLogs)
+    var tableBody=""
+    data.forEach(element => {
+      tableBody+=`<tr>
+                    <td>${element.Numero1}</td>
+                    <td>${element.Numero2}</td>
+                    <td>${element.Operacion}</td>
+                    <td>${element.Resultado}</td>
+                    <td>${element.Fecha}</td>
+                  </tr>`
+    });
+
+    tablaLogs.current.innerHTML=tableBody;
+  }
+
 
   return (
     
@@ -28,7 +168,7 @@ function App() {
         <div className="App-titulo">
           <label  >Calculadora Basica SO1 202010223</label>
           <div className="App-resultado">
-            <label >Resultado</label>
+            <label id='LabelResultado' ref={LabelResultado}>Resultado</label>
           </div>
         </div>
 
@@ -42,12 +182,12 @@ function App() {
           
           <div style={{marginTop:'40px', marginBottom:'40px'}}>
             <div style={{marginLeft:'0px'}} role="group">
-              <button type="button" class="btn btn-outline-primary letraBoton" style={{marginLeft:'10px'}}>+</button>
-              <button type="button" class="btn btn-outline-success letraBoton" style={{marginLeft:'10px'}}>-</button>
+              <button onClick={suma} type="button" class="btn btn-outline-primary letraBoton" style={{marginLeft:'10px'}}>+</button>
+              <button onClick={resta} type="button" class="btn btn-outline-success letraBoton" style={{marginLeft:'10px'}}>-</button>
             </div>
             <div style={{marginLeft:'90px', marginTop:'10px'}} role="group">
-              <button type="button" class="btn btn-outline-danger letraBoton" style={{marginLeft:'10px'}}>*</button>
-              <button type="button" class="btn btn-outline-warning letraBoton" style={{marginLeft:'10px'}}>/</button>
+              <button onClick={multiplicacion} type="button" class="btn btn-outline-danger letraBoton" style={{marginLeft:'10px'}}>*</button>
+              <button onClick={division} type="button" class="btn btn-outline-warning letraBoton" style={{marginLeft:'10px'}}>/</button>
             </div>
           </div>
 
@@ -59,9 +199,20 @@ function App() {
 
         </header>
       </div>
-
-      <div className="App-titulo">
-          <label>Aqui va a ir la tabla XDXDXD</label>
+      <button onClick={logs} type="button" class="btn btn-warning letraBoton2" style={{marginLeft:'10px'}}>Logs almacenados</button>
+      <div style={{width:"80%", marginTop:"30px"}}>
+        <table class="table table-dark table-hover" >
+          <thead>
+            <tr>
+              <th scope="col">Numero 1</th>
+              <th scope="col">Numero 2</th>
+              <th scope="col">Operación</th>
+              <th scope="col">Resultado</th>
+              <th scope="col">Fecha y hora</th>
+            </tr>
+          </thead>
+          <tbody id="tregistros" ref={tablaLogs}></tbody>
+        </table>
       </div>
     </div>
   );
