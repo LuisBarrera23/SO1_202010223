8a861809af14256aff982c8aea18023b485d8f13
LuisBarrera23 <luisbarrera5662@gmail.com>
2023-02-17 00:21:24 -0600

    pruebas de compose

--- backend/main.go
+++ backend/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"net/http"
 	"strconv"
 	"time"
+
+	_ "github.com/go-sql-driver/mysql"
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
@@ -32,16 +35,73 @@ type registros struct {
 	Fecha     string
 }
 
-var operaciones []registros
+func conectarBaseDeDatos() (conexion *sql.DB) {
+	nombreContenedor := "mysql-db"
+	nombreDB := "logs"
+	driver := "mysql"
+	usuario := "root"
+	contrasena := "1234"
+	puerto := "3306"
+
+	conexion, err := sql.Open(driver, usuario+":"+contrasena+"@tcp("+nombreContenedor+":"+puerto+")/"+nombreDB)
+	if err != nil {
+		panic(err.Error())
+	}
+	return conexion
+}
+
+func guardarOperacion(datos registros) {
+	DBconexion := conectarBaseDeDatos()
+	datalogs, err := DBconexion.Prepare("INSERT INTO registros(numero1,numero2,operacion,resultado,fecha) VALUES('" + datos.Numero1 + "','" + datos.Numero2 + "','" + datos.Operacion + "','" + datos.Resultado + "','" + datos.Fecha + "');")
+	if err != nil {
+		panic(err.Error())
+	}
+	datalogs.Exec()
+	DBconexion.Close()
+
+}
 
 func getVerificacion(res http.ResponseWriter, req *http.Request) {
 	res.Header().Set("Content-Type", "application/json")
-	enviar := saludo{Saludo: "Bienvenido el servidor esta ejecutandose correctamente!!"}
-	json.NewEncoder(res).Encode(enviar)
+	respuesta := saludo{Saludo: "Bienvenido el servidor esta ejecutandose correctamente!!"}
+	json.NewEncoder(res).Encode(respuesta)
 }
 
 func endPointRegistros(res http.ResponseWriter, req *http.Request) {
 	res.Header().Set("Content-Type", "application/json")
+	DBconexion := conectarBaseDeDatos()
+	datalogs, err := DBconexion.Query("SELECT * FROM registros")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	var operaciones []registros
+
+	for datalogs.Next() {
+		nuevo := registros{}
+		var n1 string
+		var n2 string
+		var s string
+		var r string
+		var d string
+		var id string
+		err = datalogs.Scan(&id, &n1, &n2, &s, &r, &d)
+		if err != nil {
+			respuesta := resultado{Resultado: "Error"}
+			res.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(res).Encode(respuesta)
+			return
+		}
+		nuevo.Numero1 = n1
+		nuevo.Numero2 = n2
+		nuevo.Operacion = s
+		nuevo.Resultado = r
+		nuevo.Fecha = d
+		operaciones = append(operaciones, nuevo)
+
+	}
+
+	DBconexion.Close()
 	json.NewEncoder(res).Encode(operaciones)
 }
 
@@ -66,7 +126,7 @@ func endPointSuma(res http.ResponseWriter, req *http.Request) {
 	actual := time.Now()
 	actualstring := actual.Format("02/01/2006 15:04:05")
 	registro := registros{Numero1: entrada.Numero1, Numero2: entrada.Numero2, Operacion: entrada.Simbolo, Resultado: suma, Fecha: actualstring}
-	operaciones = append(operaciones, registro)
+	guardarOperacion(registro)
 	res.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(res).Encode(respuesta)
 
@@ -92,7 +152,7 @@ func endPointResta(res http.ResponseWriter, req *http.Request) {
 	actual := time.Now()
 	actualstring := actual.Format("02/01/2006 15:04:05")
 	registro := registros{Numero1: entrada.Numero1, Numero2: entrada.Numero2, Operacion: entrada.Simbolo, Resultado: resta, Fecha: actualstring}
-	operaciones = append(operaciones, registro)
+	guardarOperacion(registro)
 	res.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(res).Encode(respuesta)
 
@@ -118,7 +178,7 @@ func endPointMultiplicacion(res http.ResponseWriter, req *http.Request) {
 	actual := time.Now()
 	actualstring := actual.Format("02/01/2006 15:04:05")
 	registro := registros{Numero1: entrada.Numero1, Numero2: entrada.Numero2, Operacion: entrada.Simbolo, Resultado: multiplicacion, Fecha: actualstring}
-	operaciones = append(operaciones, registro)
+	guardarOperacion(registro)
 	res.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(res).Encode(respuesta)
 
@@ -132,6 +192,10 @@ func endPointDivision(res http.ResponseWriter, req *http.Request) {
 	var respuesta resultado
 	if error1 != nil || error2 != nil || valor2 == 0 {
 		respuesta := resultado{Resultado: "Error"}
+		actual := time.Now()
+		actualstring := actual.Format("02/01/2006 15:04:05")
+		registro := registros{Numero1: entrada.Numero1, Numero2: entrada.Numero2, Operacion: entrada.Simbolo, Resultado: "error", Fecha: actualstring}
+		guardarOperacion(registro)
 		res.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(res).Encode(respuesta)
 		return
@@ -144,7 +208,7 @@ func endPointDivision(res http.ResponseWriter, req *http.Request) {
 	actual := time.Now()
 	actualstring := actual.Format("02/01/2006 15:04:05")
 	registro := registros{Numero1: entrada.Numero1, Numero2: entrada.Numero2, Operacion: entrada.Simbolo, Resultado: division, Fecha: actualstring}
-	operaciones = append(operaciones, registro)
+	guardarOperacion(registro)
 	res.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(res).Encode(respuesta)
 
